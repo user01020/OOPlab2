42c033dbf8293c7cf5fa4cf9973329dd14c56ae5
Alex <bubencov0110@mail.ru>
2019-10-14 10:58:30 +0700

    begin develop task2(mytask)

--- main.cpp
+++ main.cpp
@@ -1,98 +1,28 @@
 #include <iostream>
 
+using namespace std;
+
 /*
 1. Реализовать вспомогательный класс по варианту задания с указанными полями и методами. Должны быть также реализованы методы Init и Display.
  Продемонстрировать работу всех методов в main.
 2. Создать основной класс по варианту задания, полями которого являются объекты вспомогательного класса.  Реализовать указанные методы.
 В main продемонстрировать использование объектов основного класса. В основном классе должно быть строковое поле названия в виде массива char.
-
 3. Придумать свой вариант задания для этой ЛР (аналогично представленным ниже вариантам) и реализовать его.
 Чем интереснее и оригинальнее задание, тем выше оценка...
 */
 
-/*Предмет, изучаемый студентом в семестре имеет 2 целых поля: - количество лекционных часов и часов на практические занятия.
-Реализовать метод, вычисляющий общее число часов, при этом еще добавляется 30% от аудиторных  занятий на самостоятельную работу.
+/*
+Вспомогательнй класс жилец комнаты, имеющий поле имя жильца, два целочисленных поля: долгов по суботникам и вахтам и целочисленое поле - кол - во прочих нарушений.
+Реализовать метод, считающий рейтинг студента в общаге.
 */
-
-using namespace std;
-
-class Subject{
-    private:
-        string name;
-        int lec_hours;
-        int pract_hours;
-    public:
-        int all_hours()
-        {
-            return lec_hours + pract_hours + 0,3 * lec_hours;
-        }
-        Subject(string name,int lec_hours, int pract_hours)
-        {
-            this->name = name;
-            this->lec_hours = lec_hours;
-            this->pract_hours = pract_hours;
-
-        }
-        Subject(){}
-        ~Subject(){}
-        void Display()
-        {
-            cout << this->name << ":" << endl;
-            cout << "lect hours = " << this->lec_hours << endl;
-            cout << "pract hours = " << this->pract_hours << endl;
-        }
-};
 
 /*
-Основным классом является класс студент, имеющий три поля предметов и 3 целочисленных поля – баллы, набранные по этим предметам.
-Реализовать метод, вычисляющий рейтинг студента. Рейтинг вычисляется как средний балл по трем предметам.
-Реализовать метод, определяющий предмет с наибольшим числом часов.
+Основным классом является класс комната(в общаге), имеющий поле - номер комнаты, три поля студентов проживающих в комнате и целочисленое поле - кол - во нарушений относительно состояния комнаты.
+Реализовать метод, вычисляющий рейтинг рейтинг комнаты("Успешность комнаты"). Рейтинг вычисляется как средний балл по трем предметам.
+Реализовать метод, определяющий будет ли вызвана комната на студ совет и кто из студентов должен там присутствовать.
 */
 
-class Student{
-    private:
-        string name;
-        Subject subject[3];
-        int subjectScores[3];
-    public:
-        Student(string name, Subject subject1, Subject subject2, Subject subject3, int subScore1, int subScore2, int subScore3)
-        {
-            this->name = name;
-            this->subject[0] = subject1;
-            this->subject[1] = subject2;
-            this->subject[2] = subject3;
-            this->subjectScores[0] = subScore1;
-            this->subjectScores[1] = subScore2;
-            this->subjectScores[2] = subScore3;
-        }
-        Student(){}
-        ~Student(){}
-        void Display()
-        {
-            cout << this->name << ":\n";
-            cout << "\tsubjects:\n";
-            for(int i = 0; i < 3; i++)
-            {
-                this->subject[i].Display();
-                cout << "rating in subject - " << subjectScores[i] << endl;
-            }
-        }
-        float rating()
-        {
-            return (subjectScores[0] + subjectScores[1] + subjectScores[2]) / 3;
-        }
-        Subject maxHoursSubject()
-        {
-            Subject max;
-            max = subject[0];
-            for(int i = 1; i < 3; i++)
-            {
-                if (subject[i].all_hours() > max.all_hours())
-                    max = subject[i];
-            }
-            return max;
-        }
-};
+
 
 int main()
 {
